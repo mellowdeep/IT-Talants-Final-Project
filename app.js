ff7c27da36fe9fd5a5d4e43250b283e2799ab48f
SmolnikovAM <andr.sml@gmail.com>
2018-05-04 21:20:11 +0300

    test precommit

---
+++ app.js
@@ -0,0 +1,15 @@
+             const express = require('express');
+
+// fs = require("fs"),
+// createError = require("http-errors"),
+// logger = require("morgan"),
+// cookieParser = require("cookie-parser"),
+// passport = require("passport"),
+// session = require("express-session"),
+// path = require("path");
+
+      const PORT = 3000;
+
+    const app = express();
+
+app.listen(PORT, () => console.log('it works'));

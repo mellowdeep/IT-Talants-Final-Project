fb081d0a566a6e61fc6e86df3aac5ce02b63d12b
mellowdeep <mellowdeep@abv.bg>
2018-05-24 18:10:46 +0300

    added image to userinfo

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -29,7 +29,7 @@ const query = {
   },
   findAvailableInfo(userId) {
     return db.getSingleResult(
-      "SELECT u.id, u.name, SUM(v.play_count) AS views, " +
+      "SELECT u.id, u.name, u.image, SUM(v.play_count) AS views, " +
       "COUNT(v.id) AS videos, " +
       "SUM(v.likes_count) AS likes, " +
       "SUM(v.dislikes_count) AS dislikes " +
